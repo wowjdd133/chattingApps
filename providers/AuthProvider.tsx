cc4fe1d2206e9bc73e5c229e5a2259f74ec6d197
wowjdd133 <wowjddl133@naver.com>
2020-09-17 20:30:55 +0900

    [Update] authprovider
    유틸 분리.

--- providers/AuthProvider.tsx
+++ providers/AuthProvider.tsx
@@ -7,11 +7,6 @@ import ProfileScreen from "../components/screens/ProfileScreen";
 export type Auth = {
   user: any;
   setUser: any;
-  login: (email: string, password: string) => Promise<void>;
-  register: (email: string, password: string, name: string, comment:string) => Promise<void>;
-  logout: () => Promise<void>;
-  loginWithFacebook: () => Promise<void>;
-  getProfile: () => Promise<User | null>;
 }
 
 interface User {
@@ -36,75 +31,6 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
       value={{
         user,
         setUser,
-        login: async (email: string, password: string): Promise<void> => {
-          try {
-            await firebase.auth().signInWithEmailAndPassword(email, password);
-          } catch (e) {
-            console.log(e);
-          }
-        },
-        register: async (email: string, password: string, name:string, comment: string): Promise<void> => {
-          try {
-            await firebase.auth().createUserWithEmailAndPassword(email, password);
-            await firebase.auth().currentUser!.updateProfile({
-              displayName: name,
-              photoURL: "https://i.pinimg.com/736x/2c/2c/60/2c2c60b20cb817a80afd381ae23dab05.jpg",
-            })
-            const uid = firebase.auth().currentUser.uid;
-            await firebase.database().ref('users/').child(uid).set({
-              email: email,
-              uid: uid,
-              password: password,
-              name: name,
-              comment: comment,
-              profile: "https://i.pinimg.com/736x/2c/2c/60/2c2c60b20cb817a80afd381ae23dab05.jpg",
-            })
-          } catch (e) {
-            Alert.alert("실패", e.message);
-          }
-        },
-        logout: async (): Promise<void> => {
-          try {
-            await auth().signOut();
-            setUser(null);
-          } catch (e) {
-            console.log(e);
-          }
-        },
-        loginWithFacebook: async (): Promise<void> => {
-          await Facebook.initializeAsync(
-            '944503832643236'
-          );
-
-          const {
-            type,
-            token
-          } = await Facebook.logInWithReadPermissionsAsync(
-              { permissions: ['public_profile'] }
-            );
-
-          if (type === 'success') {
-            const credential = firebase.auth.FacebookAuthProvider.credential(token);
-
-            firebase.auth().signInWithCredential(credential).catch((err) => {
-              console.warn(err);
-            });
-
-          }
-        },
-        getProfile: async (): Promise<User | null> => {
-          const db = firebase.database();
-          const uid = firebase.auth().currentUser.uid;
-          const ref = db.ref("/users/" + uid);
-          let result = null;
-          ref.on("value", (snapshot: any) => {
-            result = snapshot.val();
-          }, (errorObject: any) => {
-            console.log(errorObject.code);
-          });
-
-          return result;
-        }
       }}
     >
       {children}
