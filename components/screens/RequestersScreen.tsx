b4bd34cc9173a3d1a0d1b8bc784c6ca187a20fe9
wowjdd133 <wowjddl133@naver.com>
2020-09-21 15:12:05 +0900

    [Update] RequesterScreen
    Alert, getRequesters에 setState넘겨주게 변경.

--- components/screens/RequestersScreen.tsx
+++ components/screens/RequestersScreen.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { View } from 'react-native';
+import { View, Alert } from 'react-native';
 import { Text } from 'react-native-paper';
 import { FlatList } from 'react-native-gesture-handler';
-import { getRequesters } from '../../utils/userUtil';
+import { getRequesters, responseFriend } from '../../utils/userUtil';
 import { AuthContext } from '../../providers/AuthProvider';
 import UserListItem from '../common/UserListItem';
 
 interface User {
   comment: string;
-  email: string;
   name: string;
-  password: string;
   profile: string;
   uid: string;
 }
@@ -23,14 +21,29 @@ const RequestersScreen = () => {
 
   React.useEffect(() => {
     (async () => {
-      const data = await getRequesters(authContext!.user.uid);
-
-      setRequesters(data);
+      await getRequesters(authContext!.user.uid, setRequesters);
     })();
   },[])
 
-  const handleOnPress = () => {
-
+  const handleOnPress = (uid:string,reqUid:string) => {
+    
+    Alert.alert("친구 요청이 왔습니다.", 
+    "수락하시겠습니까?",
+    [
+      {
+        text:'거절',
+        onPress:async () => {
+          await responseFriend(uid,reqUid,false);
+        }
+      },
+      {
+        text:'수락',
+        onPress:async () => {
+          await responseFriend(uid,reqUid,true);
+        }
+      }
+    ]
+    )
   }
 
   return(
@@ -40,7 +53,8 @@ const RequestersScreen = () => {
       <View
         style={{flex:1}}
       >
-        <FlatList
+        {requesters ? <FlatList
+          keyExtractor={item => item.uid}
           data={requesters}
           renderItem={({item}) => {
             return (
@@ -50,7 +64,10 @@ const RequestersScreen = () => {
               />
             )
           }}
-        />
+        /> : <Text
+          accessibilityStates
+        >없네요</Text>}
+        
       </View>
     </SafeAreaView>
   )
