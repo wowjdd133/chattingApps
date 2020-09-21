8b9def101608103bba6e2a00b80782eeb71ea9a0
wowjdd133 <wowjddl133@naver.com>
2020-09-21 15:14:08 +0900

    [Update] userUtil
    requester friend 완성.
    response friend 함수 만듦.
    getRequesters child_added에서 value로 변경. async await 삭제.
    getUsers child_added에서 value, once로 변경.

--- utils/userUtil.ts
+++ utils/userUtil.ts
@@ -1,4 +1,4 @@
-import firebase, { auth } from 'firebase';
+import firebase, { auth, functions } from 'firebase';
 import { Children } from 'react';
 
 interface User {
@@ -8,10 +8,9 @@ interface User {
   profile: string;
 }
 
-export const getProfile = async ():Promise<User | null> => {
+export const getProfile = async (uid:string):Promise<User | null> => {
 
   const db = firebase.database();
-  const uid = firebase.auth().currentUser!.uid;
   const ref = db.ref("/users/" + uid);
 
   let result:User | null = null;
@@ -45,26 +44,19 @@ export const getUsers = async ():Promise<User[] | null> => {
 
   let result:User[]= [];
 
-  await ref.orderByKey().limitToLast(100).on("child_added", (snapshot: any) => {
+  await ref.orderByKey().limitToLast(100).once("value", (snapshot: any) => {
     let data = snapshot.val();
-    
-    result.push({
-      uid: data.uid,
-      comment: data.comment,
-      profile: data.profile,
-      name: data.name,
-    });
-    // console.log(data);
-    
-    // result = data.map((item:User) => {
-    //   return {
-    //     uid: item.uid,
-    //     profile: item.profile,
-    //     comment: item.comment,
-    //     name: item.name
-    //   }
-    // })
 
+    data = Object.values(data);
+    
+    result = data.map((item:User) => {
+      return {
+        uid: item.uid,
+        profile: item.profile,
+        comment: item.comment,
+        name: item.name
+      }
+    })
   }, (errorObject: any) => {
     console.warn(errorObject.code)
   })
@@ -72,32 +64,68 @@ export const getUsers = async ():Promise<User[] | null> => {
   return result;
 }
 
-export const getRequesters = async (uid:string):Promise<User[] | null> => {
-
+export const getRequesters = async (uid:string, setRequesters:any) => {
   const db = firebase.database();
-  const ref = db.ref("/users/");
+  const ref = db.ref('/users/');
 
-  let result = null;
-
-  await ref.child(`${uid}/request`).on("child_added", (snapshot: any) => {
+  ref.child(`${uid}/friends`).on('value', async (snapshot) => {
+    let result = [];
     const data = snapshot.val();
-    const key = snapshot.key
-    
-    console.log(data);
-  }, (errorObject: any) => {
-    console.log(errorObject.code);
-  });
 
-  console.log(result);
-  return result;
+    for (const requesterID in data) {
+      if(data[requesterID]) {
+        console.log(data[requesterID]);
+        continue;
+      }
+
+      const userData = await getProfile(requesterID);
+      
+      if(userData != null)
+        result.push(userData);
+    }
+
+    setRequesters(result);
+  })
+}
+
+export const responseFriend = async (uid:string, reqUid:string, response:boolean):Promise<Boolean> => {
+  try{
+    const db = firebase.database();
+    const ref = db.ref(`/users/${uid}/friends`);
+
+    if(response){
+      let data = {};
+      data[`${reqUid}`] = true;
+
+      await ref.update(data);
+    } else {
+      await ref.child(reqUid).remove();
+    }
+
+    return true;
+    
+  }catch(err){
+    console.warn(err);
+    return false;
+  }
 }
 
 export const requestFriend = async (uid:string, reqUid:string):Promise<Boolean> => {
   
-  const db = firebase.database();
-  const ref = db.ref(`/users/${reqUid}/request`).child(uid);
-
-  await ref.update(false);
+  try{
+    const db = firebase.database();
+    const ref = db.ref(`/users/${reqUid}/friends`);
+  
+    const data = {}
+  
+    data[`${uid}`] = false;
+  
+    await ref.update(data);
+    
+  }catch(err){
+    console.log(err);
+    return false;
+  }
   
   return true;
 }
